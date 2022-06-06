ee09531c20bf29026d49b7df8c44d6c63fc10f5d
CodingFactoryT <94389494+CodingFactoryT@users.noreply.github.com>
2022-06-06 14:32:39 +0200

    chnaged example ean-number

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import main.java.Barcode_Versions.EAN13;
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
-		EAN13 barcode = new EAN13("4002846034504");	//example EAN-Number
+		EAN13 barcode = new EAN13("9312345678907");	//example EAN-Number
 		barcode.printBinary();
 		barcode.saveAs("barcode_Test");
 	}
